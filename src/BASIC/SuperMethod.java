efb7399ecd63f8c41b4cc6964c3d66252a648a09
Krish patel <krishhpatel1905@gmail.com>
2025-10-16 11:55:56 +0530

    File Added

---
+++ src/BASIC/SuperMethod.java
@@ -0,0 +1,26 @@
+package BASIC;
+class parent {
+ void display()
+ {
+    System.out.println("Hi I am Parent Class ..");
+ }
+    
+}
+
+class Child extends parent {
+    void display()
+    {
+        super.display();
+        System.out.println("Hi I am child class ");
+    }
+    
+    
+}
+
+public class SuperMethod {
+    public static void main(String[] args) {
+        Child c = new Child();
+          c.display();
+    }
+    
+}
